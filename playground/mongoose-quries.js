a73c7759beabcc8a5aa4f9b6c1977fa24be16b0a
Vinay Joshi <vinay01joshi@gmail.com>
2017-02-26 13:13:25 +0530

    Add quries playgorund file

---
+++ playground/mongoose-quries.js
@@ -0,0 +1,29 @@
+const {ObjectID} =require('mongodb')
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} =require('./../server/models/todo');
+
+var id = '58b281c7ca82e63fd05b218a';
+
+if(!ObjectID.isValid(id)){
+    console.log('Id is not valid!');
+}
+
+Todo.find({
+    _id : id
+}).then((todos)=>{
+    console.log('Todos',todos);
+});
+
+
+Todo.findOne({
+      _id : id
+}).then((todo)=>{
+    console.log('Todo',todo);
+});
+
+Todo.findById(id).then((todo)=>{
+    if(!todo){
+        return console.log('Id not found!');
+    }
+    console.log('Todo by Id',todo);
+}).catch((e)=>{console.log(e)});
