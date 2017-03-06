2e240e3b6ef06c842bcca16f3e43f370d4ec6c63
Vinay Joshi <vinay01joshi@gmail.com>
2017-03-06 13:09:12 +0530

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -103,6 +103,15 @@ UserSchema.statics.findByCredentials = function(email,password){
     });
 };
 
+UserSchema.methods.removeToken = function(token){
+    var user = this;
+   return user.update({
+        $pull :{
+            tokens:{token}
+        }
+    });
+};
+
 var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
